d37e3df168329d5fcd17124b102b5bc7cd7b090d
Maciej Kubiczek <maciej.kubiczek@teacode.io>
2022-09-15 09:05:49 +0200

    feat: add printYear method

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,111 @@
+/** @format */
+
 import * as admin from 'firebase-admin'
 import * as cheerio from 'cheerio'
 import * as request from 'request'
+import * as fs from 'fs'
 
 import { data } from '../data'
 
 let serviceAccount = require('../credentials/lcz-db-0dfaa858e54c.json')
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 })
 
 const db = admin.firestore()
 
 interface BookData {
-  id: number,
-  url: string,
-  shelfs: string[],
-  rating: number | undefined,
-  title: string,
-  author: string,
-  genre: string,
-  pages: number,
+  id: number
+  url: string
+  shelfs: string[]
+  rating: number | undefined
+  title: string
+  author: string
+  genre: string
+  pages: number
   ISBN: number | string | null
 }
 
 interface GenreData {
-  genre: string,
-  numberOfBooks: number,
+  genre: string
+  numberOfBooks: number
   books: number[]
 }
 
 interface ShelfData {
-  shelf: string,
-  numberOfBooks: number,
-  numberOfPages: number,
+  shelf: string
+  numberOfBooks: number
+  numberOfPages: number
   books: number[]
 }
 
 interface PageData {
-  book: BookData[],
-  genre: GenreData[],
+  book: BookData[]
+  genre: GenreData[]
   shelfs: ShelfData[]
 }
 
 interface InputData {
-  url: string,
-  shelfs: string[],
+  url: string
+  shelfs: string[]
   rating: number
+}
+
+const printYear = async () => {
+  const shelfsData = await getShelfsData()
+  const books = await getBooksData()
+  const data = shelfsData[8]
+
+  const numberOfBooks = data.numberOfBooks
+  const numberOfPages = data.numberOfPages
+  const shelfBooksIds = data.books
+
+  const shelfBooks = books.filter((book) => shelfBooksIds.includes(book.id))
+
+  const booksToSave = shelfBooks.map((book) => ({
+    title: book.title,
+    author: book.author,
+    genre: book.genre,
+    pages: book.pages,
+    rating: book.rating,
+  }))
+
+  console.log(' ===  2021 ===')
+  console.log('')
+  console.log('numberOfBooks', numberOfBooks)
+  console.log('numberOfPages', numberOfPages)
+  console.log('pages per book avg:', numberOfPages / numberOfBooks)
+  console.log('')
+
+  booksToSave.map((book) => {
+    console.log(`== ${book.title} ==`)
+    console.log(`author: ${book.author}`)
+    console.log(`genre: ${book.genre}`)
+    console.log(`pages: ${book.pages}`)
+    console.log(`rating: ${book.rating}`)
+    console.log('')
+  })
 }
 
 async function app() {
   const inputData: InputData = {
-    url: 'https://lubimyczytac.pl/ksiazka/32059/dziennik-nimfomanki',
+    url: 'https://lubimyczytac.pl/ksiazka/268654/zdrowie-zaczyna-sie-w-brzuchu',
     shelfs: ['Przeczytane', '2022'],
-    rating: 5
+    rating: 3
   }
   // 'Uczta wyobraźni'
   let newBookData: BookData = await getNewBookData(inputData)
 
   console.log(` == New book data == `)
   console.log(newBookData)
-  
+
   await insertNewBook(newBookData)
 
-  // updatePagesCountInShelfs(['Przeczytane', '2021'])
+  // await printYear()
+
+  // console.log('shelfBooks', shelfBooks)
+  // updatePagesCountInShelfs(['Przeczytane', '2020'])
   // udateGenres()
 }
 
@@ -71,15 +113,17 @@ const getAllDataFromFirebase = async (): Promise<PageData> => {
   return {
     book: await getBooksData(),
     genre: await getGenreData(),
-    shelfs: await getShelfsData()
+    shelfs: await getShelfsData(),
   }
 }
 
 const getBooksData = async (): Promise<BookData[]> => {
-  return await db.collection('books').get()
+  return await db
+    .collection('books')
+    .get()
     .then((data): BookData[] => {
       let books: BookData[] = []
-      data.forEach(doc => {
+      data.forEach((doc) => {
         let docData = doc.data()
         books.push({
           id: docData.id,
@@ -90,7 +134,7 @@ const getBooksData = async (): Promise<BookData[]> => {
           author: docData.author,
           genre: docData.genre,
           pages: docData.pages,
-          ISBN: docData.ISBN
+          ISBN: docData.ISBN,
         })
       })
       return books
@@ -98,15 +142,17 @@ const getBooksData = async (): Promise<BookData[]> => {
 }
 
 const getGenreData = async (): Promise<GenreData[]> => {
-  return await db.collection('genres').get()
-    .then(data => {
+  return await db
+    .collection('genres')
+    .get()
+    .then((data) => {
       let genres: GenreData[] = []
-      data.forEach(doc => {
+      data.forEach((doc) => {
         let docData = doc.data()
         genres.push({
           genre: docData.genre,
           numberOfBooks: docData.numberOfBooks,
-          books: docData.books
+          books: docData.books,
         })
       })
       return genres
@@ -114,16 +160,18 @@ const getGenreData = async (): Promise<GenreData[]> => {
 }
 
 const getShelfsData = async (): Promise<ShelfData[]> => {
-  return await db.collection('shelfs').get()
-    .then(data => {
+  return await db
+    .collection('shelfs')
+    .get()
+    .then((data) => {
       let shelfs: ShelfData[] = []
-      data.forEach(doc => {
+      data.forEach((doc) => {
         let docData = doc.data()
         shelfs.push({
           shelf: docData.shelf,
           numberOfBooks: docData.numberOfBooks,
           numberOfPages: docData.numberOfPages,
-          books: docData.books
+          books: docData.books,
         })
       })
       return shelfs
@@ -142,8 +190,10 @@ const getNewBookData = async (inputData: InputData): Promise<BookData> => {
     // title: $('meta[property="og:title"]').attr('content'),
     author: $('a.link-name').text(),
     genre: $('li.breadcrumb-item').slice(2).children('a').text(),
-    pages: Number($('#book-details dl dt:contains("Liczba stron:")').next().text()),
-    ISBN: $('meta[property="books:isbn"]').attr('content')
+    pages: Number(
+      $('#book-details dl dt:contains("Liczba stron:")').next().text()
+    ),
+    ISBN: $('meta[property="books:isbn"]').attr('content'),
   }
 }
 
@@ -167,40 +217,52 @@ const insertNewBook = async (bookData: BookData) => {
 }
 
 const deleteBookFromShelf = async (shelf: string, bookData: BookData) => {
-  let shelfData: ShelfData = await db.collection('shelfs').doc(shelf).get()
+  let shelfData: ShelfData = await db
+    .collection('shelfs')
+    .doc(shelf)
+    .get()
     .then((doc): ShelfData => {
       let data = doc.data()
       return {
         shelf: data.shelf,
         numberOfBooks: data.numberOfBooks,
         numberOfPages: data.numberOfPages,
-        books: data.books
+        books: data.books,
       }
     })
-  await db.collection('shelfs').doc(shelf).update({
-    numberOfBooks: shelfData.numberOfBooks--,
-    numberOfPages: shelfData.numberOfPages - bookData.pages,
-    books: admin.firestore.FieldValue.arrayRemove(bookData.id)
-  })
+  await db
+    .collection('shelfs')
+    .doc(shelf)
+    .update({
+      numberOfBooks: shelfData.numberOfBooks--,
+      numberOfPages: shelfData.numberOfPages - bookData.pages,
+      books: admin.firestore.FieldValue.arrayRemove(bookData.id),
+    })
   console.log(` == Book deleted from shelf: ${shelf} == `)
 }
 
 const addBookToShelf = async (shelf: string, bookData: BookData) => {
-  let shelfData: ShelfData = await db.collection('shelfs').doc(shelf).get()
+  let shelfData: ShelfData = await db
+    .collection('shelfs')
+    .doc(shelf)
+    .get()
     .then((doc): ShelfData => {
       let data = doc.data()
       return {
         shelf: data.shelf,
         numberOfBooks: data.numberOfBooks,
         numberOfPages: data.numberOfPages,
-        books: data.books
+        books: data.books,
       }
     })
-  await db.collection('shelfs').doc(shelf).update({
-    numberOfBooks: shelfData.numberOfBooks + 1,
-    numberOfPages: shelfData.numberOfPages + bookData.pages,
-    books: admin.firestore.FieldValue.arrayUnion(bookData.id)
-  })
+  await db
+    .collection('shelfs')
+    .doc(shelf)
+    .update({
+      numberOfBooks: shelfData.numberOfBooks + 1,
+      numberOfPages: shelfData.numberOfPages + bookData.pages,
+      books: admin.firestore.FieldValue.arrayUnion(bookData.id),
+    })
   console.log(` == Book added to shelf: ${shelf} == `)
 }
 
@@ -210,36 +272,39 @@ const updatePagesCountInShelfs = async (shelfsToUpdate: string[]) => {
 
   let numberOfPages: number[] = shelfsToUpdate.map((shelf): number => {
     let pagesNumber = 0
-    shelfs.find((el: ShelfData) => el.shelf === shelf).books.forEach((bookId) => {
-      pagesNumber += books.find((el: BookData) => el.id === bookId).pages
-    })
+    shelfs
+      .find((el: ShelfData) => el.shelf === shelf)
+      .books.forEach((bookId) => {
+        pagesNumber += books.find((el: BookData) => el.id === bookId).pages
+      })
     return pagesNumber
   })
 
   for (let id in shelfsToUpdate) {
     db.collection('shelfs').doc(shelfsToUpdate[id]).update({
-      numberOfPages: numberOfPages[id]
+      numberOfPages: numberOfPages[id],
     })
     console.log(` == Shelf pages count updated: ${shelfsToUpdate[id]} == `)
   }
 }
 
 const updateGenres = async () => {
-  const shelfData: ShelfData = await db.collection('shelfs').doc('Przeczytane').get()
-    .then(data => {
+  const shelfData: ShelfData = await db
+    .collection('shelfs')
+    .doc('Przeczytane')
+    .get()
+    .then((data) => {
       let docData = data.data()
       return {
         shelf: docData.shelf,
         numberOfBooks: docData.numberOfBooks,
         numberOfPages: docData.numberOfPages,
-        books: docData.books
+        books: docData.books,
       }
     })
 
   let promises = []
-  promises.push(shelfData.books.map(async bookId => {
-
-  }))
+  promises.push(shelfData.books.map(async (bookId) => {}))
 }
 
 app()
