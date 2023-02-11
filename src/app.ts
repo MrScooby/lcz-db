b1f62fe7c3fba9bca34224a59ed9d842c55a0bbe
Maciej Kubiczek <maciej.kubiczek@gmail.com>
2023-02-11 01:14:38 +0100

    removeEmptySpacesFromTtiles

--- src/app.ts
+++ src/app.ts
@@ -52,47 +52,11 @@ interface InputData {
   rating: number
 }
 
-const printYear = async () => {
-  const shelfsData = await getShelfsData()
-  const books = await getBooksData()
-  const data = shelfsData[8]
-
-  const numberOfBooks = data.numberOfBooks
-  const numberOfPages = data.numberOfPages
-  const shelfBooksIds = data.books
-
-  const shelfBooks = books.filter((book) => shelfBooksIds.includes(book.id))
-
-  const booksToSave = shelfBooks.map((book) => ({
-    title: book.title,
-    author: book.author,
-    genre: book.genre,
-    pages: book.pages,
-    rating: book.rating,
-  }))
-
-  console.log(' ===  2021 ===')
-  console.log('')
-  console.log('numberOfBooks', numberOfBooks)
-  console.log('numberOfPages', numberOfPages)
-  console.log('pages per book avg:', numberOfPages / numberOfBooks)
-  console.log('')
-
-  booksToSave.map((book) => {
-    console.log(`== ${book.title} ==`)
-    console.log(`author: ${book.author}`)
-    console.log(`genre: ${book.genre}`)
-    console.log(`pages: ${book.pages}`)
-    console.log(`rating: ${book.rating}`)
-    console.log('')
-  })
-}
-
 async function app() {
   const inputData: InputData = {
-    url: 'https://lubimyczytac.pl/ksiazka/268654/zdrowie-zaczyna-sie-w-brzuchu',
-    shelfs: ['Przeczytane', '2022'],
-    rating: 3
+    url: 'https://lubimyczytac.pl/ksiazka/4937141/ikabog',
+    shelfs: ['Przeczytane', '2023'],
+    rating: 5
   }
   // 'Uczta wyobraźni'
   let newBookData: BookData = await getNewBookData(inputData)
@@ -100,13 +64,31 @@ async function app() {
   console.log(` == New book data == `)
   console.log(newBookData)
 
-  await insertNewBook(newBookData)
+  // await insertNewBook(newBookData)
 
   // await printYear()
-
   // console.log('shelfBooks', shelfBooks)
   // updatePagesCountInShelfs(['Przeczytane', '2020'])
   // udateGenres()
+}
+
+const removeEmptySpacesFromTtiles = async () => {
+  const booksRef = db.collection('books');
+  const snapshot = await booksRef.get();
+  const regex = /^\s+|\s+$/gm
+
+  for (let i = 0; i < snapshot.docs.length; i++) {
+    const bookDoc = snapshot.docs[i]
+    const bookData = bookDoc.data()
+
+    const regexReturn = regex.test(bookData.title)
+
+    if (regexReturn) {
+      const newTitle = bookData.title.trim()
+      console.log(newTitle)
+      await db.collection('books').doc(bookDoc.id).update({title: newTitle})
+    }
+  }
 }
 
 const getAllDataFromFirebase = async (): Promise<PageData> => {
@@ -186,7 +168,7 @@ const getNewBookData = async (inputData: InputData): Promise<BookData> => {
     url: inputData.url,
     shelfs: inputData.shelfs,
     rating: inputData.rating,
-    title: $("h1.book__title").text().substring(1),
+    title: $("h1.book__title").text().substring(1).trim(),
     // title: $('meta[property="og:title"]').attr('content'),
     author: $('a.link-name').text(),
     genre: $('li.breadcrumb-item').slice(2).children('a').text(),
@@ -305,6 +287,42 @@ const updateGenres = async () => {
 
   let promises = []
   promises.push(shelfData.books.map(async (bookId) => {}))
+}
+
+const printYear = async () => {
+  const shelfsData = await getShelfsData()
+  const books = await getBooksData()
+  const data = shelfsData[8]
+
+  const numberOfBooks = data.numberOfBooks
+  const numberOfPages = data.numberOfPages
+  const shelfBooksIds = data.books
+
+  const shelfBooks = books.filter((book) => shelfBooksIds.includes(book.id))
+
+  const booksToSave = shelfBooks.map((book) => ({
+    title: book.title,
+    author: book.author,
+    genre: book.genre,
+    pages: book.pages,
+    rating: book.rating,
+  }))
+
+  console.log(' ===  2021 ===')
+  console.log('')
+  console.log('numberOfBooks', numberOfBooks)
+  console.log('numberOfPages', numberOfPages)
+  console.log('pages per book avg:', numberOfPages / numberOfBooks)
+  console.log('')
+
+  booksToSave.map((book) => {
+    console.log(`== ${book.title} ==`)
+    console.log(`author: ${book.author}`)
+    console.log(`genre: ${book.genre}`)
+    console.log(`pages: ${book.pages}`)
+    console.log(`rating: ${book.rating}`)
+    console.log('')
+  })
 }
 
 app()
